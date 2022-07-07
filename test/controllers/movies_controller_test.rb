20a0b6ee9fc3cfcf9f3d87444027de596245c506
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-07 02:23:57 -0300

    rails test

---
+++ test/controllers/movies_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class MoviesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_movies_url
+    assert_response :success
+  end
+
+  test "should get movie genre" do
+    get api_v1_movies_url, params: { genre: "Action" }
+    assert_response :success
+  end
+
+  test "should get movie year" do
+    get api_v1_movies_url, params: { year: "2022" }
+    assert_response :success
+  end
+
+  test "should get movie country" do
+    get api_v1_movies_url, params: { country: "USA"}
+    assert_response :success
+  end
+end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid movie' do
+    movie = Movie.new(title: 'Top Gun: Maverick',
+                      year: '2022',
+                      country: 'USA',
+                      genre: 'Action',
+                      published_at: '2022-05-26',
+                      description: "Pete 'Maverick' Mitchell is brought back to teach a new band of TopGun academy
+                                    graduates how to run a dangerous mission to destroy a uranium facility.")
+    assert movie.valid?
+  end
 end
