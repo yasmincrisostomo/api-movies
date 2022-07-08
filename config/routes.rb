625377d40b2c5013097f0763a9ae03a79ff3ad42
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-08 19:26:02 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api, format: :json do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :movies, only: %i[index create]
     end
   end
-  root to: 'api/v1/movies#index', format: :json
+  root to: 'api/v1/movies#index', defaults: { format: :json }
 end
