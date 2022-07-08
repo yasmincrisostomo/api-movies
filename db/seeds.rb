34fabe485125a882c1e51ab32ee5162803ed75e2
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-08 14:21:52 -0300

    uuid

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ require 'csv'
 csv_path = File.read(Rails.root.join('lib', 'csv', 'netflix_titles.csv'))
 csv = CSV.parse(csv_path, headers: true)
 csv.each do |row|
-  Movie.create({  title: row['title'],
+  Movie.create({  id: SecureRandom.uuid,
+                  title: row['title'],
                   genre: row['type'],
                   year: row['release_year'],
                   country: row['country'],
