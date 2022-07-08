5d02ea19c73cab9f933867207eb43fa54eab58e4
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-08 14:09:23 -0300

    limit and offset

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::MoviesController < Api::V1::BaseController
   def index
-    @movies = params[:query].present? ? Movie.search(params[:query]) : Movie.all.order(:year)
+    limit = params[:limit] || 100
+    offset = params[:offset] || 0
+    if params[:query].present?
+      @movies = Movie.search(params[:query]).limit(limit).offset(offset)
+    else
+      @movies = Movie.all.order(:year).limit(limit).offset(offset)
+    end
+    # @movies = params[:query].present? ? Movie.search(params[:query]).limit(limit).offset(offset) : Movie.all.order(:year).limit(limit).offset(offset)
   end
 end
