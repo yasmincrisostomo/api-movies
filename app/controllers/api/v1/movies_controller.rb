67ae222e7990073a45bfb2013ff8447e62a106b0
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-07-08 18:42:36 -0300

    post file

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Api::V1::MoviesController < Api::V1::BaseController
   def index
     limit = params[:limit] || 100
@@ -6,6 +8,18 @@ class Api::V1::MoviesController < Api::V1::BaseController
       @movies = Movie.search(params[:query]).limit(limit).offset(offset)
     else
       @movies = Movie.all.order(:year).limit(limit).offset(offset)
+    end
+  end
+
+  def create
+    CSV.foreach(params[:file], headers: true).each do |row|
+      Movie.create({  id: SecureRandom.uuid,
+                      title: row['title'],
+                      genre: row['type'],
+                      year: row['release_year'],
+                      country: row['country'],
+                      published_at: row['date_added'],
+                      description: row['description'] })
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :movies, only: [:index]
-      resources :movies, only: [:index]
+      resources :movies, only: %i[index create]
     end
   end
   root to: 'api/v1/movies#index', defaults: { format: :json }
